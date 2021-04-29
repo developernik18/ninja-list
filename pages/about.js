7e9019f36047a1f7b3865caae6eb389df77d7089
Nikhil Kumar <developernik18@gmail.com>
2021-04-29 17:43:41 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <> 
+      <Head>
+        <title>
+          Ninja List | about
+        </title>
+        <meta name="keywords" content="about"/>
+      </Head>
+      <div>
+        <h2>About any topic</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolores eum error odit cupiditate assumenda soluta recusandae iure. Possimus eaque maxime id nobis, beatae quaerat. Adipisci ut vitae qui quis.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolores eum error odit cupiditate assumenda soluta recusandae iure. Possimus eaque maxime id nobis, beatae quaerat. Adipisci ut vitae qui quis.</p>
+
+      </div>
+    </>
+   );
+}
+ 
+export default About;
